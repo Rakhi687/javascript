1fc84ce662654c50f3ee7fa10ff2bcf979218bf2
Rakhi687 <pradhanrakhi038@gmail.com>
2025-01-06 02:12:10 +0000

    variable

---
+++ basics/datatype.js
@@ -0,0 +1,14 @@
+// number 
+// BigInt
+// String
+// Boolean
+// null(standalone value)
+// undefined
+// Symbol(uniqueness)
+
+
+
+// Object
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
---
+++ basics/variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountmail = "rakhi@gmail.com"
+var accountpassword = "1234"
+accountcity = "Odisha"
+let accountstate;
+
+
+// accountId = 123
+accountmail = "rak@gmail.com"
+accountpassword = "12"
+accountcity = "Bengaluru"
+
+
+console.table([accountId,accountmail,accountpassword,accountcity])
+
+console.log(accountId,accountmail,accountpassword,accountcity);
