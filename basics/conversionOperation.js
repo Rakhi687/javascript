13687d95780f6f013be4125c8bfdf3b291f1b658
Rakhi687 <pradhanrakhi038@gmail.com>
2025-01-06 02:57:08 +0000

    conversionOperations

---
+++ basics/conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = true;
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);//it will convert to number
+
+// console.log(valueInNumber);//NaN not a number as score is 33abc
+
+// if score is "33" then it convert to number with type number
+
+// if score is 33abc then it type is number but value is NaN
+
+// if score = null type is number value is 0
+
+// if score = undefined type is number value is NaN
+
+// if scote=true type is number value is 1
+
+
+let booleanValue = 0
+let Value = Boolean(booleanValue)
+console.log(Value);
+//if 0 then value false,except 0 it will give true
+let sNumber = 99
+let someNumber = String(sNumber)
+console.log(typeof someNumber);
+
