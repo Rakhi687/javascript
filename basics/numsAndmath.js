584401a9a84bb2a114e0d25d96e830cdecedb7c6
Rakhi687 <pradhanrakhi038@gmail.com>
2025-01-07 17:43:26 +0530

    Create numsAndmath.js
    changed

---
+++ basics/numsAndmath.js
@@ -0,0 +1,43 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));         //to get decimal value ,after decimal number of digit
+
+
+const otherNumber = 123.8966 
+// console.log(otherNumber.toPrecision(4));   //to get until the 4 digit number
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); // to get in format of local for easy understanding
+
+//##########################    Maths      #########################
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.max(4, 9));
+// console.log(Math.min(3, 4));
+
+
+
+console.log(Math.random() * 10 + 1);
+console.log(Math.floor(Math.random()*10+1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
