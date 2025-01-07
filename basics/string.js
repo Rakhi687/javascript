9d39fcb0b33a9c681626bfb5328804c092f63819
Rakhi687 <pradhanrakhi038@gmail.com>
2025-01-07 17:42:13 +0530

    maths and nums

---
+++ basics/string.js
@@ -0,0 +1,42 @@
+let name = "Rakhi"
+let count="90"
+console.log(`hello my name is ${name} and my repo count is ${count}`);
+
+
+
+ const name1 = new String("Rakhi-how- are you are you doing- good")
+// console.log(name1.__proto__);
+// console.log(name1.length);
+// console.log(name1.toUpperCase());
+// console.log(name1.charAt(0));
+// console.log(name1.indexOf('k'));
+const newstr = name1.substring(0, 3)
+// console.log(newstr);
+const newstr2 = name1.slice(4,-2)
+// console.log(newstr2);
+
+
+const newstring = "    priyanka       "
+// console.log(newstring);
+// console.log(newstring.trim());//delete the whitespace
+const url = "https://rakhi.com/rakhi%20pradhan"
+console.log(url.replace('%20', '-'))
+console.log(url.includes('nandu'));
+
+
+console.log(name1.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
