01d9eb8f40b1567c0ecc085604763b2d85e697b2
Rakhi687 <pradhanrakhi038@gmail.com>
2025-01-07 08:34:51 +0530

    done conversion

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -21,9 +21,37 @@
 
 let booleanValue = 0
 let Value = Boolean(booleanValue)
-console.log(Value);
+//console.log(Value);
 //if 0 then value false,except 0 it will give true
 let sNumber = 99
 let someNumber = String(sNumber)
-console.log(typeof someNumber);
+//console.log(typeof someNumber);
+
+++sNumber;
+//console.log(sNumber);
+ 
+
+// Normal comaprison
+
+// console.log(2 > 1);
+// console.log(2 <= 1);
+
+//different datatype
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//Null value comparison
+
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null>0);
+
+//same for undefined
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined>0);
+
+
+
 
