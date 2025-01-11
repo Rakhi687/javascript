438d9c7d91ca06f461ec9a07a223bd76f9688cf2
Rakhi687 <pradhanrakhi038@gmail.com>
2025-01-11 06:42:43 +0000

    SingletonObjects

--- basics/array.js
+++ basics/array.js
@@ -4,40 +4,40 @@ const myarr = [1, 2, 34, 5]
 const myarr2 = new Array(1, 2, 3, 4, 5)
 // console.log(myarr2);
 
-myarr2.push(6)
-myarr2.pop(6)
+// myarr2.push(6)
+// myarr2.pop(6)
 
 // console.log(myarr2);
-myarr.unshift(9)
-myarr.shift()
+// myarr.unshift(9)
+// myarr.shift()
 // console.log(myarr.includes(9));
 // console.log(myarr.includes(5));
 // console.log(myarr.indexOf(2));
 const newarr=myarr.join()
-// console.log(newarr);
-// console.log(myarr);
+console.log(newarr);
+console.log(myarr);
 
 //slice
 // console.log(myarr);
-const myn1 = myarr.slice(1, 3)//it give subset of the range
-// console.log(myn1);
-const myn2 = myarr.splice(0,2)//it delete that perticular range of number
-// console.log(myn2);
+// const myn1 = myarr.slice(1, 3)//it give subset of the range
+// // console.log(myn1);
+// const myn2 = myarr.splice(0,2)//it delete that perticular range of number
+// // console.log(myn2);
 
 
-const my_hero = ["shaktiman", "hanuman", "lalman"]
-const marvel_hero = ["thor", "batman", "superman"]
-// marvel_hero.push(my_hero)
-// console.log(marvel_hero);
-const new_arr = marvel_hero.concat(my_hero)
-// console.log(new_arr);
-const all_newheros = [...marvel_hero, ...my_hero]    //spread an array
-// console.log(all_newheros);
-let Array = [1, 2, 3,[4, 5], 6, [7, 8, 9]]
-let new_array=aray.flat(Infinity)
-// console.log(new_array);
-// console.log(Array.isArray("rakhi"));
-// console.log(Array.from("rakhi"));
+// const my_hero = ["shaktiman", "hanuman", "lalman"]
+// const marvel_hero = ["thor", "batman", "superman"]
+// // marvel_hero.push(my_hero)
+// // console.log(marvel_hero);
+// const new_arr = marvel_hero.concat(my_hero)
+// // console.log(new_arr);
+// const all_newheros = [...marvel_hero, ...my_hero]    //spread an array
+// // console.log(all_newheros);
+// let Array = [1, 2, 3,[4, 5], 6, [7, 8, 9]]
+// let new_array=aray.flat(Infinity)
+// // console.log(new_array);
+// // console.log(Array.isArray("rakhi"));
+// // console.log(Array.from("rakhi"));
 
 
 
---
+++ basics/objects02.js
@@ -0,0 +1,76 @@
+//singletonObjects
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "rak@123"
+tinderUser.name = "rakhi"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+
+const regularUser = {
+    email: "rakhi@gmail.com",
+    fullname: {
+        userfullname: {              //nested objects
+            firstname: "Rakhi",
+            lastname:"Pradhan"
+            
+        }
+    }
+}
+
+
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = { 1: "a", 2: "b", 3: "d" }
+const obj2 = { 3: "c", 5: "p", 6: "u" }     //used to combine objects by their value and key
+
+const obj3 = Object.assign({}, obj1, obj2)
+const obj4 = {...obj1,...obj2,...obj3}//spread the values
+// console.log(obj4); 
+
+// console.log(obj3);
+
+//objects in array
+
+const users = [
+    {
+        name: "Rakhi",
+        mail:"rak@gmail.com"
+    },
+    {
+        phno: 8926034445,
+        class: "7th"
+    },
+    {
+        age:18
+    }
+]
+// console.log(users[0].mail); //to retrive element of an array with objects
+
+// console.log(Object.keys(regularUser));  // to retive the keys of objects
+
+// console.log(Object.values(regularUser));
+
+
+// console.log(regularUser.hasOwnProperty("email"));
+
+// console.log(regularUser.hasOwnProperty("mail"));
+
+//OBJECT DE-STRUCTURING
+const course = {
+    coursename:"javascript",
+    coursefees:"999",
+    courseInstructor:"Hitesh"
+}
+
+//TO EXTRACT MULTIPLE VALUE
+console.log(course.coursefees);
+
+const { courseInstructor:cinst } = course
+console.log(cinst);  //TO DEMODIFY THE VALUE OF THE OBJECT BY UR OWN VARIABLE
+
+
+
