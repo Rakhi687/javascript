cdda926e4a99070542a57f5a85904ca2b280257f
Rakhi687 <pradhanrakhi038@gmail.com>
2025-01-25 04:53:50 +0000

    immediatly invoked function expression

---
+++ 02_basics/IIFE.js
@@ -0,0 +1,8 @@
+(function chai(num1, num2) {
+    console.log(num1 + num2)
+})(3, 4);
+
+     ( () => { 
+         console.log("Hello World!")
+     })()
+     //WE HAVE TO ADD A SEMICOLON TO END THE FUNCTION";"
