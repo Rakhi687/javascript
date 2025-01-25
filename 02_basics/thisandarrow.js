0be88704bf87c0459c7334d923a360c080b9102e
Rakhi687 <pradhanrakhi038@gmail.com>
2025-01-25 03:25:11 +0000

    add it

--- 02_basics/thisandarrow.js
+++ 02_basics/thisandarrow.js
@@ -4,7 +4,7 @@ const user = {
     price: 999,
     welcomeMessage: function () { console.log(`${this.username},welcome to website`);
     
-    console.log(this);//IT WILL PRINT THE CURRENT VALUE OBJECT RAKHI,999THEN SAM,999
+    // console.log(this);//IT WILL PRINT THE CURRENT VALUE OBJECT RAKHI,999THEN SAM,999
 
     }
     
@@ -13,26 +13,26 @@ const user = {
 // user.welcomeMessage()
 //  user.username = "sam"
 // user.welcomeMessage()
-// console.log(this);
+// console.log(this); IT REFERS TO AN EMPTY OBJECT
 
 //GLOBAL OBJECT IN THE BROWSER IS WINDOW OBJECT
 
 //standalone JavaScript engine IT EXECUTE THE CODE OUTSIDE THE BROWSER
 
-// function chai() {
-//     const username="rakhi"        //OUTPUT:-UNDEFINED
-//     console.log(this.username);
+ function chai() {
+    const username="rakhi"        //OUTPUT:-UNDEFINED
+     console.log(this.username);
+    
+ } //CANT USE THIS OBJECT IN FUNCTION 
+//  chai()
+
+
+
+// const chai = function () {
+//     const username = "rakhi"
+//     console.log(this.username);     //OUTPUT:-UNDEFINED
     
 // }
-// chai()
-
-
-
-const chai = function () {
-    const username = "rakhi"
-    console.log(this.username);     //OUTPUT:-UNDEFINED
-    
-}
 //chai()
 ////////////////ARROW FUNCTION////////////////
 
@@ -44,7 +44,7 @@ const one = () => {
     
 
 }
-one()
+// one()
 
 //TYPE OF DEFINING ARROW FUNCTION
 const Two = (num1, num2) => { 
@@ -58,9 +58,34 @@ const three = (num1, num2) => (num1 + num2)
 console.log(three(2, 9))
 
 
-const four = () => ({ username: "Rakhi" })
-console.log(four());
+// const four = () => ({ username: "Rakhi" })
+// console.log(four());
 
+
+function john() { 
+    const name = "Rakhi";
+    let course = "MCA"
+    console.log(this.name)
+    
+}
+john()
+
+const priya = () => {
+    const name = "Rakhi"
+    console.log(this);
+    
+}
+priya();
+
+
+const adding = (num1, num2) => num1 + num2
+console.log(adding(3, 4));
+
+const obj = () => ({
+    username:"Rakhi"
+}    
+)
+console.log(obj())
 
 
 
