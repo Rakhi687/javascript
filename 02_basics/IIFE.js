a030e550cc692c11a597dab8a52d40b765395c4f
Rakhi687 <pradhanrakhi038@gmail.com>
2025-01-29 01:46:14 +0000

    controlflow

--- 02_basics/IIFE.js
+++ 02_basics/IIFE.js
@@ -2,7 +2,8 @@
     console.log(num1 + num2)
 })(3, 4);
 
-     ( () => { 
-         console.log("Hello World!")
-     })()
-     //WE HAVE TO ADD A SEMICOLON TO END THE FUNCTION";"
+(() => {
+    console.log("Hello World!")
+})()
+//WE HAVE TO ADD A SEMICOLON TO END THE FUNCTION";"
+     
---
+++ controlflow/p1.js
@@ -0,0 +1,43 @@
+// if (3===2) {
+//     console.log("its working");
+
+// }
+// else { 
+//     console.log("not working");
+
+// }
+// const bal=1000
+// if (bal < 500) console.log("lessthan")
+
+// if (bal < 500) {
+//     console.log("less than");
+
+// } else if (bal < 750) { 
+//     console.log("less than 750");
+
+// } 
+// else{
+//     console.log("less than 1200")
+// } 
+//ternary operator
+// const price=100
+// price>=80 ? console.log("yes"):console.log("No")
+//switch case
+const month = "jan"
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("Febraury");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    default:
+        console.log("not a month");
+        break;
+
+}
+
+
